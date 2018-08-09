00cc479bc7d515caa5af125dbf104f8581cdf997
William Queiroz <wnqueiroz@outlook.com>
2018-08-09 16:06:59 -0300

    Changed Users component to handle with Loading component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
+import Loading from './Loading'
 
 import { getUsers } from '../services/api'
 
 class Users extends Component {
 
+    state = {
+        loading: false
+    }
+
     getUsers = async () => {
-        const response = await getUsers()
+        this.setState({ loading: true })
+        const response = await getUsers().then(response => {
+            this.setState({ loading: false })
+            return response
+        })
         console.log({ response })
     }
 
     render() {
+        const { loading } = this.state
+
         return (
-            <button onClick={this.getUsers}>Buscar usuários</button>
+            <Fragment>
+                <button onClick={this.getUsers}>Buscar usuários</button>
+                <Loading loading={loading} message='Carregando usuários...' />
+            </Fragment>
         )
     }
 }
