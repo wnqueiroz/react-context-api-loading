a1ffbe4226e3f04609c329bb5f3132591ed99bee
William Queiroz <wnqueiroz@outlook.com>
2018-08-09 15:59:49 -0300

    Added styles to Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React, { Component, Fragment } from 'react'
 import Departments from './components/Departments'
 import Users from './components/Users'
 
+import './index.css'
+
 class App extends Component {
 	render() {
 		return (
