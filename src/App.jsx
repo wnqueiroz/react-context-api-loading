16aa04505b30a3a3cf83f8d81794f795fdedddd4
William Queiroz <wnqueiroz@outlook.com>
2018-08-09 22:12:47 -0300

    Using Context API

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,58 @@ import React, { Component, Fragment } from 'react'
 
 import Departments from './components/Departments'
 import Users from './components/Users'
+import Loading from './components/Loading'
 
 import './index.css'
 
 const LoadingContext = React.createContext({
 	loading: false,
 	message: null,
-	showMessage: message => { },
+	showLoading: message => { },
 	hideLoading: () => { }
 })
 
 class App extends Component {
+
+	state = {
+		loading: false,
+		message: null
+	}
+
+	showLoading = message => this.setState({
+		loading: true,
+		message
+	})
+
+	hideLoading = () => this.setState({ loading: false })
+
 	render() {
+		const { showLoading, hideLoading } = this
+
+		const value = {
+			...this.state,
+			showLoading,
+			hideLoading
+		}
+
 		return (
-			<Fragment>
-				<Users />
-				<Departments />
-			</Fragment>
+			<LoadingContext.Provider value={value}>
+				<LoadingContext.Consumer>
+					{
+						({ showLoading, hideLoading, loading, message }) => (
+							<Fragment>
+
+								<Users  {...{ showLoading, hideLoading }} />
+
+								<Departments {...{ showLoading, hideLoading }} />
+
+								<Loading {...{ loading, message }} />
+
+							</Fragment>
+						)
+					}
+				</LoadingContext.Consumer>
+			</LoadingContext.Provider>
 		)
 	}
 }
